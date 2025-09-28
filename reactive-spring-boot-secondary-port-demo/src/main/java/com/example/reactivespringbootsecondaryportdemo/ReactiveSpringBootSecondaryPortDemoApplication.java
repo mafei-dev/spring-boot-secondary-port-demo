52aac5bc54b9cee25189f5931c46a81b259d2e0b
mafei <mafei.dev@gmail.com>
2025-09-28 22:47:47 +0530

    Add Maven wrapper and project configuration for reactive and servlet Spring Boot applications

---
+++ reactive-spring-boot-secondary-port-demo/src/main/java/com/example/reactivespringbootsecondaryportdemo/ReactiveSpringBootSecondaryPortDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.reactivespringbootsecondaryportdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringBootSecondaryPortDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSpringBootSecondaryPortDemoApplication.class,
+                              args);
+    }
+
+}
---
+++ reactive-spring-boot-secondary-port-demo/src/test/java/com/example/reactivespringbootsecondaryportdemo/ReactiveSpringBootSecondaryPortDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivespringbootsecondaryportdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringBootSecondaryPortDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
